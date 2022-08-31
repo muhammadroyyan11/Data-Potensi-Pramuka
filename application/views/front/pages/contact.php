cb448ac16415a0db2f0d8a940ad54ede2fcf7f91
muhammadroyyan11 <royyanmz87@gmail.com>
2022-09-01 00:24:40 +0700

    update about

--- application/views/front/pages/contact.php
+++ application/views/front/pages/contact.php
@@ -63,8 +63,14 @@
                 </div><br><br>
                 <div class="row">
 
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <!-- Start Map -->
+                        <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro suscipit voluptatem corporis illum totam, molestias fugiat nemo nam. Iste enim cupiditate nostrum nisi eveniet corporis? Odit a modi iusto molestias, consequuntur libero rerum enim voluptatum assumenda ratione sint recusandae hic alias sunt tempora, suscipit rem. Accusamus voluptatum soluta eum, cumque esse tempora nesciunt non, minus saepe laudantium iusto unde ab amet neque. Adipisci voluptatibus quia deserunt inventore suscipit assumenda enim placeat, amet iusto optio aut reiciendis quibusdam earum commodi impedit molestias repudiandae cum perspiciatis provident similique odit! Temporibus, atque accusantium.</p>
+                        <!-- End Map -->
+                    </div>
+
                     <!-- Start Google Map -->
-                    <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="col-md-6 col-sm-6 col-xs-12">
                         <!-- Start Map -->
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126968.60811563936!2d105.92164196249999!3d-6.026412399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e418e700c41ff03%3A0x6d27e85972f1572f!2sGerakan%20Pramuka%20Kwartir%20Ranting%20Cilegon!5e0!3m2!1sid!2sid!4v1661966357084!5m2!1sid!2sid" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
                         <!-- End Map -->
